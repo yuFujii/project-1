f4f00ff04ae10b07f053a5287816616ab7d4a685
Daichi Ninomiya <daichi.ninomiya@deer.co.jp>
2018-12-11 06:31:43 +0900

    トップへ戻るボタンデザイン

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
-    <a v-scroll-to="'#top'">
+    <a
+      v-scroll-to="'#top'"
+      class="to-top"
+    >
       <i class="material-icons">keyboard_arrow_up</i>
     </a>
     <footer class="footer">
@@ -9,10 +12,29 @@
   </div>
 </template>
 
-<style lang="scss">
+<style lang="scss" scoped>
 .footer {
   text-align: center;
   font-size: 12px;
   margin: 32px 0;
+}
+
+.to-top {
+  position: fixed;
+  bottom: 24px;
+  right: 24px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: #fff;
+  border-radius: 50%;
+  width: 50px;
+  height: 50px;
+  box-shadow: 0 2px 10px #ddd;
+  cursor: pointer;
+  transition: 0.5s;
+  &:hover {
+    transform: translateY(-10px);
+  }
 }
 </style>

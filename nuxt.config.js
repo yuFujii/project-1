70bfaf8d185767b03bcc9780c5f1776e3b6d16d0
Daichi Ninomiya <daichi.ninomiya@deer.co.jp>
2018-12-19 09:41:08 +0900

    remove noto sans

--- nuxt.config.js
+++ nuxt.config.js
@@ -71,10 +71,6 @@ module.exports = {
       {
         rel: 'stylesheet',
         href: 'https://fonts.googleapis.com/icon?family=Material+Icons'
-      },
-      {
-        rel: 'stylesheet',
-        href: 'https://fonts.googleapis.com/css?family=Noto+Sans+JP'
       }
     ]
   },
